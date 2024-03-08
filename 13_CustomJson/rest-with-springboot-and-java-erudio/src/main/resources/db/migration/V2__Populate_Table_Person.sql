559e3e20e045dc9d2ee6c1312351dd978295d6de
Davi Antunes <davi-gomes182@outlook.com>
2024-03-08 15:21:35 -0300

    Custom Json serializable, mudando os nomes das propriedades no postman

---
+++ 13_CustomJson/rest-with-springboot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,12 @@
+INSERT INTO `tb_person` (`id`, `adress`, `first_name`, `gender`, `last_name`) VALUES 
+	(1,'São Paulo - SP','Ayrton','Male','Senna'),
+	(2,'Anchiano','Leonardo','Male','da Vinci'),
+	(4,'Porbandar - India','Indira','Female','Gandhi'),
+	(5,'Porbandar - India','Mahatma','Male','Gandhi'),
+	(7,'Miami - US','David','Male','Backham'),
+	(8,'Kentucky - US','Muhammad','Male','Ali'),
+	(9,'Batel - CWB','Bianca','Female','Baggio'),
+	(10,'Rio de Janeiro - RJ','Ronaldinho','Male','Gaúcho'),
+	(11,'Ilha da Madeira - PT','Cristiano','Male','Ronaldo'),
+	(12,'Mvezo - South Africa','Nelson','Male','Mandela'),
+	(13,'Smiljan - Croatia','Nikola','Male','Tesla');
